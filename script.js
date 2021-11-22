cc1d63f95545c4000bbe055712f1a70925ffce02
fthbby <fthbby@gmail.com>
2021-11-21 18:11:31 -0700

    class for StatsBar added

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 console.log ('hello')
+
+
+//------- attempt to create the stats bar buttons wtih classes ----//
+
+class StatsBar {
+    constructor(barSize, color) {
+        this.barSize = barSize;
+        this.color = color;
+
+    }
+}
+
+
+const energy = new StatsBar(100, 'green')
+const hunger = new StatsBar(0,'yellow')
+const boredom = new StatsBar(0,'orange')
+
+console.log(energy)
+console.log(hunger)
+console.log(boredom)
