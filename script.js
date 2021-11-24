ed2e3c63d5a7897b7773766611d7d83a64ed3293
fthbby <fthbby@gmail.com>
2021-11-23 23:29:15 -0700

    got all the stats bars to move in unison!

--- script.js
+++ script.js
@@ -4,33 +4,54 @@ console.log ('hello')
 //------- attempt to create the stats bar buttons wtih classes ----//
 
 
-class StatsBar {
-    constructor(x,y,w,h,maxBarSize,color) {
-        this.x =x;
-        this.y = y;
-        this.w = w;
-        this.h = h;
-        this.maxBarSize = maxBarSize;
-        this.color = color;
-        this.maxWidth = w;
-        this.energy = maxBarSize;
-        this.hunger = maxBarSize;
-        this.boredom = maxBarSize;
+// class StatsBar {
+//     constructor(x,y,w,h,maxBarSize,color) {
+//         this.x =x;
+//         this.y = y;
+//         this.w = w;
+//         this.h = h;
+//         this.maxBarSize = maxBarSize;
+//         this.color = color;
+//         this.maxWidth = w;
+//         this.energy = maxBarSize;
+//         this.hunger = maxBarSize;
+//         this.boredom = maxBarSize;
 
-    }
+//     }
 
-}
-
-
-const energyBar = new StatsBar(20,20,200,30,10000, 'green')
+// }
 
 
+// const energyBar = new StatsBar(20,20,200,30,10000, 'green')
 
 
-const statsBar = document.getElementsByClassName('stats-bar')[0];
+
+
+const boredBar = document.getElementById('bored-bar');
+const hungerBar = document.getElementById('hunger-bar');
+const energyBar = document.getElementById('energy-bar');
 
 setInterval(() => {
-    const computedStyle = getComputedStyle(statsBar);
+    const computedStyle = getComputedStyle(boredBar);
     const width = parseFloat(computedStyle.getPropertyValue('--width')) || 0
-    statsBar.style.setProperty('--width', width +.05)
+    boredBar.style.setProperty('--width', width +.05)
 }, (5));
+
+setInterval(() => {
+    const computedStyle = getComputedStyle(hungerBar);
+    const width = parseFloat(computedStyle.getPropertyValue('--width')) || 0;
+    hungerBar.style.setProperty('--width', width +.05)
+}, (5));
+
+setInterval(() => {
+    const computedStyle = getComputedStyle(energyBar);
+    const width = parseFloat(computedStyle.getPropertyValue('--width')) || 0;
+    energyBar.style.setProperty('--width', width +.05)
+}, (5));
+
+
+
+// let hungerBar = '';
+// let boredomBar = '';
+// let energyBar = '';
+
