a36bd0b161dc26c578a796bc0e3a95bcc2b39679
fthbby <fthbby@gmail.com>
2021-11-23 23:37:00 -0700

    combined all the progress bar intervals into one function (DRY!)

--- script.js
+++ script.js
@@ -31,27 +31,35 @@ const boredBar = document.getElementById('bored-bar');
 const hungerBar = document.getElementById('hunger-bar');
 const energyBar = document.getElementById('energy-bar');
 
-setInterval(() => {
-    const computedStyle = getComputedStyle(boredBar);
-    const width = parseFloat(computedStyle.getPropertyValue('--width')) || 0
-    boredBar.style.setProperty('--width', width +.05)
-}, (5));
+// setInterval(() => {
+//     const computedStyle = getComputedStyle(boredBar);
+//     const width = parseFloat(computedStyle.getPropertyValue('--width')) || 0
+//     boredBar.style.setProperty('--width', width +.05)
+// }, (5));
 
-setInterval(() => {
-    const computedStyle = getComputedStyle(hungerBar);
-    const width = parseFloat(computedStyle.getPropertyValue('--width')) || 0;
-    hungerBar.style.setProperty('--width', width +.05)
-}, (5));
+// setInterval(() => {
+//     const computedStyle = getComputedStyle(hungerBar);
+//     const width = parseFloat(computedStyle.getPropertyValue('--width')) || 0;
+//     hungerBar.style.setProperty('--width', width +.05)
+// }, (5));
 
-setInterval(() => {
-    const computedStyle = getComputedStyle(energyBar);
-    const width = parseFloat(computedStyle.getPropertyValue('--width')) || 0;
-    energyBar.style.setProperty('--width', width +.05)
-}, (5));
+// setInterval(() => {
+//     const computedStyle = getComputedStyle(energyBar);
+//     const width = parseFloat(computedStyle.getPropertyValue('--width')) || 0;
+//     energyBar.style.setProperty('--width', width +.05)
+// }, (5));
 
 
+function intervalStart (chooseBar,interval) {
+    setInterval(() => {
+        const computedStyle = getComputedStyle(chooseBar);
+        const width = parseFloat(computedStyle.getPropertyValue('--width')) || 0;
+        chooseBar.style.setProperty('--width', width +.05)
+    }, (interval));
+}
 
-// let hungerBar = '';
-// let boredomBar = '';
-// let energyBar = '';
+// why are the numbers opposite??? isnt higher number supposed to be faster?? need to figure this one
 
+intervalStart(boredBar, 5)
+intervalStart(hungerBar, 1)
+intervalStart(energyBar, 10)
