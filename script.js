a8cf5a22c53272433d85109eba7535120d5e342c
fthbby <fthbby@gmail.com>
2021-11-30 19:05:38 -0700

    got both winner and loser screens to go back to welcome page to play again

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let cat = {
     count: 0,
     
 }
-let countProgress = 0;
+// let countProgress = 0;
 
 const boredBar = document.getElementById('bored-bar-progress');
 const energyBar = document.getElementById('energy-bar-progress');
@@ -30,8 +30,10 @@ function startCount(){
     countProgress = setInterval(()=>{
     cat.count++;
     counter.innerText = cat.count;
-        if (cat.count >= 10){
+        if (cat.count >= 10){ //------ change timer here
             console.log('winner!')
+            showWinnerPage();
+            gameEnds();
             clearInterval(countProgress)
         }
     }, 1500); //------ this changes speed of counter..
@@ -40,23 +42,6 @@ function startCount(){
 // document.addEventListener('click',function(){
 //     console.log(count)
 // })
-
-
-//------- use as test
-function intervalStartHunger(){
-    hungerBarProgress = setInterval(function () {
-        cat.hunger++;
-        $('#hunger-bar').css('width', cat.hunger +'%');
-        if (cat.hunger >= 100){
-            console.log('this hungerBar works')
-            // resetHunger();
-            clearInterval(hungerBarProgress);
-            gameEnds();
-            showLoserPage();
-            //------game over here
-        }
-    }, 100)
-}
 
 //----------below are the event listeners for the action buttons
 
@@ -175,9 +160,6 @@ const clickActionNegative = (chooseBar) => {
 
 
 
-console.log($('.loser-page'))
-
-
 function playGame(){
     // clearLoserPage();
     startCount(0); //-------this one starts the counter,
@@ -187,9 +169,7 @@ function playGame(){
     clearWelcomePage();
 }
 
-// const welcomePage = document.querySelector('.welcome-page');
-// const welcomeStart = document.querySelector('.welcome-start')
-// const nameBox = document.querySelector('#name-box');
+
 const catName = document.querySelector('#catName');
 const gameStartButton = document.querySelector('.gameStartButton')
 
@@ -209,19 +189,34 @@ function clearLoserPage() {
 }
 function showLoserPage() {
     $('.loser-page').css('display','inline');
+    $('.winner-page').css('display','none');
+    $('.try-again').css('display','inline')
+
+
 }
 
 
 function showWinnerPage(){
-    $('.winner-page').css('display','inline')
-}
-// gameStartButton.addEventListener('submit', playGame())
+    $('#playAgainButton').css('display','flex')
 
+    $('.winner-page').css('display','inline');
+    $('.loser-page').css('display','none');
+    clearWelcomePage();
+    clearInterval(hungerBarProgress);
+    clearInterval(energyBarProgress);
+    clearInterval(boredBarProgress);
+
+}
+
+function clearWinnerPage(){
+    $('.winner-page').css('display','none');
+}
 
 gameStartButton.addEventListener('click', function(e){
     if (e.target.innerText =='Enter'){
         cat.name = $('#catName').val();
         $('#name-box').text(cat.name)
+        // $('.try-again').css('display','none')
         // addNameToGame();
         // clearInterval(intervalStartEnergy)
         // clearInterval(intervalStartHunger)
@@ -230,22 +225,52 @@ gameStartButton.addEventListener('click', function(e){
     }
 })
 
-const playAgainButton = document.querySelector('.playAgainButton')
+// const playAgainButton = document.querySelector('.playAgainButton')
+const $playAgainButton = $('.playAgainButton')
 
-console.log(playAgainButton)
-
-playAgainButton.addEventListener('click', function(e){
-    if (e.target.innerText =='Try again!'){
-
+$playAgainButton.on('click', function(e){
+    if (e.target.innerText =='Play again!'){
+        e.preventDefault();
+        console.log('clickie')
+        clearWinnerPage();
         clearLoserPage();
         $('body').css('visibility','hidden');
         location.reload();
-        return false;
-        
-
+        // return false;
 
     }
 }
 )
 
 
+// playAgainButton.addEventListener('click', function(e){
+//     if (e.target.innerText =='Play again!'){
+//         e.preventDefault();
+//         console.log('clickie')
+//         clearWinnerPage();
+//         clearLoserPage();
+//         $('body').css('visibility','hidden');
+//         location.reload();
+//         // return false;
+
+//     }
+// }
+// )
+
+
+
+
+
+// playAgainButton.addEventListener('click', function(e){
+//     if (e.target.innerText =='Try again!'){
+//         e.preventDefault();
+//         console.log('clickie')
+//         clearWinnerPage();
+//         clearLoserPage();
+//         $('body').css('visibility','hidden');
+//         location.reload();
+//         // return false;
+
+//     }
+// }
+// )
